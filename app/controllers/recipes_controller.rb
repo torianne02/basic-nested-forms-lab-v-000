6dec6df2eb433d918f6cb8787306e87b3e117ef2
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-08-21 17:39:05 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -18,7 +18,7 @@ class RecipesController < ApplicationController
     redirect_to recipe_path(@recipe)
   end
 
-  def person_params 
+  def recipe_params 
     params.require(:recipe).permit(
       :title,
       :ingredients_attributes: [
