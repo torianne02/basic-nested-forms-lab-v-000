eac9cb1eb1e4160880b87cd09e64dfe8ce5ec686
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-08-21 17:40:26 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -21,7 +21,7 @@ class RecipesController < ApplicationController
   def recipe_params 
     params.require(:recipe).permit(
       :title,
-      :ingredients_attributes: [
+      ingredients_attributes: [
         :name,
         :quantity
       ]
